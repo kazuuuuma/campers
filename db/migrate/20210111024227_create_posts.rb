595f17f652038f4e33333370c74e5bbe1f21de26
kazuuuuma <kazuma.hayakawa.1024@gmail.com>
2021-01-11 15:27:36 +0900

    gem追加

---
+++ db/migrate/20210111024227_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.text :text
+      t.integer :place_id, null: false
+      t.integer :toilet_id,null: false
+      t.integer :fire_id,  null: false
+      t.integer :river_id, null: false
+      t.integer :gomi_id,  null: false
+      t.integer :water_id, null: false
+      t.string :price,     null: false
+      t.references :user,   foreign_key: true
+      t.timestamps
+    end
+  end
+end
